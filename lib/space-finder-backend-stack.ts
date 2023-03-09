a025442984298bfb9797a300f70bad34c3327f25
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-03-09 10:44:31 +0000

    lambda-nodejs for typescript. .ts file

--- lib/space-finder-backend-stack.ts
+++ lib/space-finder-backend-stack.ts
@@ -2,8 +2,11 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { GenericTable } from './GenericTable';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { join } from 'path';
+
+
 
 export class SpaceFinderBackendStack extends cdk.Stack {
 
@@ -12,7 +15,7 @@ export class SpaceFinderBackendStack extends cdk.Stack {
     'SpacesTable',
     'spaceId',
     this
-)
+  )
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -23,6 +26,11 @@ export class SpaceFinderBackendStack extends cdk.Stack {
       code: lambda.Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
     })
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+      handler: 'handler'
+    });
 
 
     // Hello Api lambda integration:
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,11 +1,11 @@
-// import { v4 } from 'uuid'
+import { v4 } from 'uuid'
 
 
-// async function handler(event: any, context: any) {
-//     return {
-//         statusCode: 200,
-//         body: 'Hello from lambda!' + v4()
-//     }
-// }
+async function handler(event: any, context: any) {
+    return {
+        statusCode: 200,
+        body: 'Hello from lambda!' + v4()
+    }
+}
 
-// export { handler }
+export { handler }
