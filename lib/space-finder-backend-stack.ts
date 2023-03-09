fd48736d3aed688bda43f817f6ecf433a8ee7800
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-03-09 12:29:45 +0000

    install and use aws-sdk, list buckets using lamba function, give permision to lamba to view s3 buckets, Add policy to Lambda function

--- lib/space-finder-backend-stack.ts
+++ lib/space-finder-backend-stack.ts
@@ -3,6 +3,7 @@ import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import {PolicyStatement} from '@aws-cdk/aws-iam'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { join } from 'path';
 
@@ -25,6 +26,10 @@ export class SpaceFinderBackendStack extends cdk.Stack {
       entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
       handler: 'handler'
     });
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*')
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
 
     // Hello Api lambda integration:
