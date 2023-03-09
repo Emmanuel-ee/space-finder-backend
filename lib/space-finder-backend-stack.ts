d8d16d6a099bb49ed3fa6e6d4afec1f898dad32f
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-03-09 09:47:51 +0000

    integrate lamba with api-gateway, test with rest-client or postman

--- lib/space-finder-backend-stack.ts
+++ lib/space-finder-backend-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
 export class SpaceFinderBackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+  private api = new apigw.RestApi(this, 'SpaceApi')
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SpaceFinderBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const helloLambda = new lambda.Function(this, 'helloLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main'
+    })
+
+
+    // Hello Api lambda integration:
+    const helloLambdaIntegration = new apigw.LambdaIntegration(helloLambda)
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
