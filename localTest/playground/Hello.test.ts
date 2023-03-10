753f2cde89633d28c4c4c10c8d7d9dc58a54afa1
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-03-10 14:22:44 +0000

    Update data in dynamodb

--- localTest/playground/Hello.test.ts
+++ localTest/playground/Hello.test.ts
@@ -1,15 +1,17 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { handler } from '../../services/SpacesTable/Read';
-
+import { handler } from '../../services/SpacesTable/Update';
 
 const event: APIGatewayProxyEvent = {
     queryStringParameters: {
-        location: 'Londonzzz'
+        spaceId: '29191766-7113-447e-b7bf-4175be2ebd11'
+    },
+    body: {
+        location: 'new location'
     }
 } as any;
 
 
-const result = handler(event, {} as any).then((apiResult)=>{
+const result = handler(event, {} as any).then((apiResult) => {
     const items = JSON.parse(apiResult.body);
     console.log(123)
 });
